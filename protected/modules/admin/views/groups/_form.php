6517dabe968b2c2ae1eb83fd022c5f0742648276
tavanchinh <chinh.tv91@gmail.com>
2016-05-24 11:11:31 +0700

    12

--- protected/modules/admin/views/groups/_form.php
+++ protected/modules/admin/views/groups/_form.php
@@ -77,63 +77,6 @@
         </div>
     </div>
     
-	<?php echo $form->errorSummary($model); ?>
-
-<<<<<<< HEAD
-	
-=======
-	<div class="row">
-		<?php echo $form->labelEx($model,'name'); ?>
-		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>255)); ?>
-		<?php echo $form->error($model,'name'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'des'); ?>
-		<?php echo $form->textField($model,'des',array('size'=>60,'maxlength'=>255)); ?>
-		<?php echo $form->error($model,'des'); ?>
-	</div>
-   
-   <div class="md-card-content">
-        <?php 
-        $list_all = Functional::model()->getMultilevel();
-        
-        if($model->id != null){
-            $list_selected = GroupFunctional::model()->findAllByAttributes(array(
-               'group_id' => $model->id,
-            ));    
-        }else{
-            $list_selected = array();
-        }
-        
-        
-        if(count($list_all) > 0){?>
-            <ul class="checklist">
-                <?php foreach($list_all as $value){?>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="categories[]" <?php echo (in_array($value['id'],$list_selected)) ? 'checked' : ''?> value="<?php echo $value['id']?>" />
-                            <?php echo $value['name']?>
-                        </label>
-                        <?php if(isset($value['sub'])){?>
-                        <ul class="children">
-                            <?php foreach($value['sub'] as $sub){?>
-                                <li>
-                                    <label>
-                                        <input type="checkbox" name="categories[]" <?php echo (in_array($sub['id'],$list_selected)) ? 'checked' : ''?> value="<?php echo $sub['id']?>" />
-                                        <?php echo $sub['name']?>
-                                    </label>
-                                </li>
-                            <?php }?>
-                        </ul>
-                        <?php }?>
-                    </li>
-                <?php }?>
-            </ul>
-        <?php }
-        ?>
-    </div>
-    
 	<div class="row buttons uk-margin-top uk-text-right">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save',array('class' => 'md-btn md-btn-primary')); ?>
 	</div>
